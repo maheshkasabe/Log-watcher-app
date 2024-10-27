364732254b1b74b5921ab8473e93ca9149a3f224
Mahesh Kasbe <mahesh@Maheshs-MacBook-Air.local>
2024-10-27 20:43:19 +0530

    pipeline log

--- test.js
+++ test.js
@@ -1,15 +1,17 @@
-var fs = require('fs');
+// var fs = require('fs');
 
-let counter = 1;
-fs.appendFile("test.log",Date.now()+" :"+counter.toString(),(err) => {
-    if(err) throw err;
-    console.log("test file initialized");
-});
-counter++;
-setInterval(function(){
-    fs.appendFile("test.log","\n"+Date.now()+": "+counter,(err) => {
-    if(err) console.log(err);
-    console.log("log updated");
-    });
-    counter++;
-},10);
+// let counter = 1;
+// fs.appendFile("test.log",Date.now()+" :"+counter.toString(),(err) => {
+//     if(err) throw err;
+//     console.log("test file initialized");
+// });
+// counter++;
+// setInterval(function(){
+//     fs.appendFile("test.log","\n"+Date.now()+": "+counter,(err) => {
+//     if(err) console.log(err);
+//     console.log("log updated");
+//     });
+//     counter++;
+// },1000);
+
+console.log("Hello World");
